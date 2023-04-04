3e0f8fdfb58624b1b5a490882f9f08b17700d001
henkistar1 <120992219+henkistar1@users.noreply.github.com>
2023-04-04 03:21:19 +0200

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -65,4 +65,11 @@ function clearAmount() {
 //   document.getElementById("shuma").value = "";
 //   document.getElementById("perqindja-amount").textContent = "";
 // }
-
+var lastTouchEnd = 0;
+document.addEventListener('touchend', function (event) {
+  var now = (new Date()).getTime();
+  if (now - lastTouchEnd <= 300) {
+    event.preventDefault();
+  }
+  lastTouchEnd = now;
+}, false);
